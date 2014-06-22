9818d4fdcd02a49e13d4ff814aec46a9a0d895ee
Eduard Netšajev <themediaisajoke@gmail.com>
2014-06-22 12:45:33 +0300

    test

--- Python/1stProject/Programm.py
+++ Python/1stProject/Programm.py
@@ -9,3 +9,8 @@ color = input("What is your favorite color?")
 
 print("Ah, so your name is %s, your quest is %s, " \
 "and your favorite color is %s." % (name, quest, color))
+
+a = 6
+print(a)
+a="Wow"
+print(a)
