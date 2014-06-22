fb574e36d45d30f3362bad2643d02f5bff25be15
Eduard Netšajev <themediaisajoke@gmail.com>
2014-06-22 21:33:30 +0300

    program improved

--- Python/CodeCademy/Pig Latin Translator.py
+++ Python/CodeCademy/Pig Latin Translator.py
@@ -1,11 +1,15 @@
 __author__ = 'Netšajev'
 
-word = input('Enter a word:')
 
-if len(word) > 0 and word.isalpha():
-    word = word.lower()
-    word = word+word[0]+"ay"
-    word = word[1:len(word)]
-    print(word)
-else:
-    print('empty')
+def translator():
+    word = input('Enter a word:')
+    if len(word) > 0 and word.isalpha():
+        word = word.lower()
+        word = word+word[0]+"ay"
+        word = word[1:len(word)]
+        print(word)
+        translator()
+    else:
+        print('empty')
+
+translator()
