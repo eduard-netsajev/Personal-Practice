fa8f81ac68a01cf691778ad2abf2e28ff8952035
Eduard Netšajev <themediaisajoke@gmail.com>
2014-06-23 12:41:17 +0300

    updated program

--- Python/CodeCademy/1stProgram.py
+++ Python/CodeCademy/1stProgram.py
@@ -26,3 +26,32 @@ if my_bool:
     greetings(name)
 print("\nToday is {:0>2}/{:0>2}/{}. Time is {:0>2}:{:0>2}:{:0>2}"
       .format(now.day, now.month, now.year, now.hour, now.minute, now.second))
+
+everything = dir(datetime)  # Sets everything to a list of things from math
+print(everything)  # Prints 'em all!
+
+
+def print_max_and_min(*args):
+    print("Maximum in array is " + str(max(*args)))
+    print("Minimum in array is " + str(min(*args)))
+
+
+print_max_and_min(-10, -5, 5, 10, 13, 12.7, 13.1)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def absolute(number):
+    if is_number(number):
+        print(abs(float(number)))
+    else:
+        print("Entered not a valid number!")
+
+
+absolute(input())
