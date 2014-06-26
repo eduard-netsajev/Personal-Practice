c3bdd3097e8bbd402316aab8758a7e66d9b08f0b
Eduard Netšajev <themediaisajoke@gmail.com>
2014-06-26 16:21:06 +0300

    GitLanguages much improved

--- Python/CodeCademy/GitLanguages.py
+++ Python/CodeCademy/GitLanguages.py
@@ -2,15 +2,19 @@ __author__ = 'Netšajev'
 
 import requests
 import json
+from datetime import datetime
+import os
+import os.path
 
-r = requests.get('https://api.github.com/repos/eduard-netsajev/Personal-Practice')
-if r.ok:
-    repoItem = json.loads(r.text or r.content)
-    print("Eduard's personal repository created: ")
-    print(repoItem['created_at'])
-    l = requests.get(repoItem['languages_url'])
+now = datetime.now()
+PATH = "git_lang_data/{:0>2}-{:0>2}-{} langs.txt".format(now.day, now.month, now.year)
+
+if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
+    print("File {} exists and is readable".format(PATH))
+else:
+    l = requests.get('https://api.github.com/repos/eduard-netsajev/Personal-Practice/languages')
     if l.ok:
-        print("\nLanguages used:")
+        print("\nLanguages used in Eduard's repository:\n")
         totalbytes = 0
         langItem = json.loads(l.text or l.content)
         sorted_list = [x for x in langItem.items()]
@@ -20,5 +24,9 @@ if r.ok:
             totalbytes += value
         for key, value in sorted_list:
             print("{0} - {1:.2%}".format(key, value/totalbytes))
-else:
-    print("He he he. Probably request limit exceeded.")
+
+        with open(PATH, 'w') as f:
+            json.dump(langItem, f, ensure_ascii=False)
+        print("\nFile {} was created with all the information".format(PATH))
+    else:
+        print("\nHe he he. Probably request limit exceeded. Or some problems with internet connection.")
