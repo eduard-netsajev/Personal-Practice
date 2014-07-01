73e2c2db61934560bd37f8df5fc7e23766221d8e
Eduard Netšajev <themediaisajoke@gmail.com>
2014-07-01 17:39:45 +0300

    advanced topics finished

--- Python/CodeCademy/AdvancedTopics.py
+++ Python/CodeCademy/AdvancedTopics.py
@@ -12,3 +12,23 @@ print(my_dict.values())
 for key in my_dict:
     print(key, my_dict[key])
 
+evens_to_50 = [i for i in range(51) if i % 2 == 0]
+print(evens_to_50)
+
+doubles_by_3 = [x*2 for x in range(1, 20) if (x*2) % 3 == 0]
+print(doubles_by_3)
+
+
+print(evens_to_50[3:10:2])
+print(evens_to_50[::-3])
+
+my_list = range(16)
+print(list(filter(lambda x: x % 3 == 0, my_list)))
+
+squares = [x**2 for x in range(1, 11)]
+print(list(filter(lambda x: 30 <= x <= 70, squares)))
+
+garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+message = (list(filter(lambda x: x != 'X', garbled)))
+message = "".join(message)
+print(message)
