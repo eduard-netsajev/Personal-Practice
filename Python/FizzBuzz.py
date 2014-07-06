67ce8b446d920b397d747638bdc2782851e4f9e7
Eduard Netšajev <themediaisajoke@gmail.com>
2014-07-06 21:16:28 +0300

    FizzBuzz improved

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -4,9 +4,14 @@ import random
 
 for i in range(100):
     num = random.randint(1, 1000)
+    print("\n{:3}".format(num), end='')
 
-    print("\n{:3} - ".format(num), end='')
-    if num % 3 == 0:
+    divided_by3 = not num % 3
+    divided_by5 = not num % 5
+
+    if divided_by3 or divided_by5:
+        print(" - ", end = '')
+    if divided_by3:
         print("Fizz", end='')
-    if num % 5 == 0:
+    if divided_by5:
         print("Buzz", end='')
