36ce4d22def0a06790959afe1e7a7d521b470b81
Eduard Netšajev <themediaisajoke@gmail.com>
2014-07-23 12:14:29 +0300

    Scraper: gets all the links to the group programs, stores in output txt file as a json list

--- Python/OISscraper/Scraper.py
+++ Python/OISscraper/Scraper.py
@@ -2,21 +2,33 @@ __author__ = 'Netšajev'
 
 from bs4 import BeautifulSoup
 import requests
+import json
 
 r = requests.get('http://ois.ttu.ee/portal/page?_pageid=35,435155&_dad=portal&_schema=PORTAL&k=&a=1&b=1&c=-1&d=-1&e=-1&e_sem=141&i=1&q=neto&g=-1')
 
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text, 'lxml')
 
-f = open('output.html', 'w')
+soup = soup.find_all('table')[-1]
+
+
+links = []
+
+for row in soup.find_all('span'):
+    link = row.get('onclick')
+    if len(link) > 50:
+        link = link[22:-15]
+        link = 'http' + link
+        print(link)
+        links.append(link)
+
+
 string = soup.prettify()
-f.write(string)
-
+f = open('links.txt', 'w')
+json.dump(links, f, ensure_ascii=False)
 f.close()
 
 print("List of all programms: ")
-
 kavad = []
-
 for link in soup.find_all('span'):
     kava = link.get('kava')
     if kava is not None and kava not in kavad:
@@ -29,11 +41,6 @@ for i in range(len(kavad)):
         print(kavad[i], end=' ')
     else:
         print(kavad[i])
-#print(link.get('href'))
-#print(soup.get_text())
 
-for tag in soup.find_all(True):
-    pass
-#    print(tag.name)
 
 print("Finished.")
