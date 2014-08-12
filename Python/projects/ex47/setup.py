b293f991f3db6c8522ea1fc5e23273cd81a57d9d
Eduard Netšajev <themediaisajoke@gmail.com>
2014-08-12 23:12:45 +0300

    Added simple exercising game with unit tests, all is working

---
+++ Python/projects/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = [
+    'description': 'My Project',
+    'author': 'Eduard Netsajev',
+    'url': 'http://github.com/eduard-netsajev',
+    'download_url': 'Where to download it.',
+    'author_email': 'themediaisajoke@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+]
+
+setup(**config)
