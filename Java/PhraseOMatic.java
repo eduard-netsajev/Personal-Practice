71546ea2d885c1b8f7360ab32be23cafff7ab162
Eduard Netšajev <themediaisajoke@gmail.com>
2014-08-27 02:50:04 +0300

    first Java IntelliJ commit, modified PhraseOMatic according to CheckStyle guidance

--- Java/PhraseOMatic.java
+++ Java/PhraseOMatic.java
@@ -1,21 +1,38 @@
+/**
+ * Main class.
+ */
 public class PhraseOMatic {
-public static void main(String[] args){
-String[] wordListOne = {"24/7","multi-Tier", "30,000 foot", "B-to-B", "win-win", "front-end","web-based","pervasive","smart","six-sigma","critical-path","dynamic"};
+    /**
+     * Simple Java practice program with funny results.
+     *
+     * @param args console parameters
+     */
+    public static void main(String[] args) {
 
-String [] wordListTwo={"empowered","sticky", "value-added","oriented","centric","disturbed","clustered","branded","outside-the-box","positioned","networked","focused","leveraged","aligned","targeted","shared","cooperative","accelerated"};
+        String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B",
+                "win-win", "front-end", "web-based", "pervasive", "smart",
+                "six-sigma", "critical-path", "dynamic"};
 
-String[] wordListThree={"process","tipping-point","solution","architecture","core comepancy","strategy","mindshare","portal","space","vision","paradigm","mission"};
+        String [] wordListTwo = {"empowered", "sticky", "value-added",
+                "oriented", "centric", "disturbed", "clustered", "branded",
+                "outside-the-box", "positioned", "networked", "focused",
+                "leveraged", "aligned", "targeted",
+                "shared", "cooperative", "accelerated"};
 
-int oneLength = wordListOne.length;
-int twoLength = wordListTwo.length;
-int threeLength = wordListThree.length;
+        String[] wordListThree = {"process", "tipping-point", "solution",
+                "architecture", "core comepancy", "strategy", "mindshare",
+                "portal", "space", "vision", "paradigm", "mission"};
 
-int rand1 = (int) (Math.random() * oneLength);
-int rand2 = (int) (Math.random() * twoLength);
-int rand3 = (int) (Math.random() * threeLength);
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
 
-	String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
 
-	System.out.println("What we need is a " + phrase);
-}
+        String phrase = wordListOne[rand1] + " "
+                + wordListTwo[rand2] + " " + wordListThree[rand3];
+System.out.println("What we need is a " + phrase);
+    }
 }
