0ce23633f12446c6588d364a230516d4cce056c8
Eduard Netšajev <themediaisajoke@gmail.com>
2014-08-27 13:16:20 +0300

    test commit

--- Java/total_novice_level/DooBee.java
+++ Java/total_novice_level/DooBee.java
@@ -1,12 +1,20 @@
+/**
+ * DooBeeDoo.
+ */
 public class DooBee {
+    /**
+     *
+     * @param args console args
+     */
     public static void main(String[] args) {
         int x = 1;
-        while (x < 3) {
+        int c = x + x + x;
+        while (x < c) {
             System.out.print("Doo");
             System.out.print("Bee");
             x = x + 1;
         }
-        if (x == 3) {
+        if (x == c) {
             System.out.print("Do");
         }
     }
