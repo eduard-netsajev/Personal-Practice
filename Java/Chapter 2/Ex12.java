b5703a49b20cd2ad379c4acf042eee51fb56f2b6
Eduard Netšajev <themediaisajoke@gmail.com>
2014-08-29 21:18:03 +0300

    Intro to Java Programming - Finished Chapter 2

---
+++ Java/Chapter 2/Ex12.java
@@ -0,0 +1,10 @@
+public class Ex12 {
+    public static void main(String[] args){
+        // If today is Tuesday, what will be the day in 100 days
+        int day = 2;
+        day += 100;
+        day %= 7;
+        System.out.println(day);
+        // day = 4, which means Thursday
+    }
+}
--- Java/Chapter 2/Ex4.java
+++ Java/Chapter 2/Ex4.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Ex2 {
+public class Ex4 {
     public static void main(String[] args){
+        final double PI = 3.14159;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a circle radius: ");
         double radius = input.nextDouble();
-        double area = radius * radius * 3.14159;
+        double area = radius * radius * PI;
 
         System.out.println("The area for the circle of radius " + radius +
                 " is " + area);
