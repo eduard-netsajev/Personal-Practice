fe4f7bbf7a9b6d1a7f3e872ec50947adbda5453f
Eduard Netšajev <themediaisajoke@gmail.com>
2014-08-30 18:22:53 +0300

    Intro to Java Programming - Finished Chapter 4

--- Java/Chapter 1/Ex4.java
+++ Java/Chapter 1/Ex4.java
@@ -1,17 +1,11 @@
-/**
- * Created by Netšajev on 28/08/2014.
- */
 public class Ex4 {
     public static void main(String[] args){
-        System.out.println("a      a^2    a^3");
+        System.out.println("a     a^2    a^3");
         for(int a = 1; a < 5; a++){
-            System.out.print(a + "      " + (a*a));
-            if (a < 4){
-                System.out.println("      " + (a*a*a));
-            }
-            else {
-                System.out.println("     " + (a*a*a));
-            }
+
+            System.out.printf("%-7d", a);
+            System.out.printf("%-7d", a*a);
+            System.out.printf("%d\n", a*a*a);
         }
     }
 }
