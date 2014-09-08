629757043af110b141578bc0d0eadaf447080ea9
Eduard Netšajev <themediaisajoke@gmail.com>
2014-09-08 23:43:27 +0300

    Chapter 11 finished

---
+++ Java/Chapter 11/Etc.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Etc {
+    public static void main(String[] args) {
+
+
+        String str = "Eduard";
+        String str1 = new String("Eduard");
+        String str2 = "Eduard";
+
+        System.out.println(str == str1);
+        System.out.println(str == str2);
+        System.out.println(str.equals(str1));
+
+        Integer[] array = {3, 5, 95, 4, 15, 34, 3, 6, 5};
+        System.out.println(java.util.Collections.max(Arrays.asList(array)));
+    }
+
+}
---
+++ Java/Chapter 11/MyStack.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class MyStack {
+
+    private ArrayList<Object> list = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public Object peek() {
+        return list.get(getSize() - 1);
+    }
+
+    public Object pop() {
+        Object temp = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return temp;
+    }
+
+    public void push(Object obj) {
+        list.add(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Stack: " + list.toString();
+    }
+
+}
