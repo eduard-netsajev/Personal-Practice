d0f158d7116b9086b5daf20bd8c9d5677a8b8e58
Eduard Netšajev <themediaisajoke@gmail.com>
2014-09-17 20:01:21 +0300

    Chapter 13 - some snippets added.

--- Java/Chapter 13/Etc.java
+++ Java/Chapter 13/Etc.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-import java.util.Calendar;
+import java.util.*;
+
 import org.jetbrains.annotations.NotNull;
-import java.util.GregorianCalendar;
 
 public class Etc {
     public static void main(String[] args) {
@@ -13,24 +12,48 @@ public class Etc {
         System.out.println(x.intValue());
         System.out.println(x.doubleValue());
 
-        Calendar calendar = new GregorianCalendar(2008, 8, 1);
+        Calendar septemberFirst = new GregorianCalendar(2008, 8, 1);
         Calendar today = new GregorianCalendar();
-        System.out.println(today.get(Calendar.WEEK_OF_YEAR) +" "+ calendar.get
-                (Calendar
-                .WEEK_OF_YEAR));
+        System.out.println(today.get(Calendar.WEEK_OF_YEAR));
+        System.out.println(septemberFirst.get (Calendar.WEEK_OF_YEAR));
 
         Person p1 = new Person();
         Person p2 = new Person();
         Person p3 = new Person();
 
         Person[] people = new Person[]{p1, p2, p3};
+        List<Person> samePeople = new ArrayList<>(Arrays.asList(people));
+
         Arrays.sort(people);
+        Collections.sort(samePeople, new PersonComparator());
+        // samePeople.sort(new PersonComparator()); - redirects to above
     }
 }
 
-class Person implements Comparable<Person> {
+class Person implements Comparable<Person>, Scream {
+    String name;
     @Override
     public int compareTo(@NotNull Person person) {
         return 0; // Socialism
     }
+
+    @Override
+    public void calmDown(Person p){
+        System.out.printf("Hey, %s, calm down please\n", p.name);
+    }
+}
+
+class PersonComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person one, Person two) {
+        return 0; // Socialism
+    }
+}
+
+interface Scream {
+    static void shout(){
+        System.out.println("AAAAAaaaaaaaaaaaaaaaaa!");
+    }
+
+    void calmDown(Person v);
 }
