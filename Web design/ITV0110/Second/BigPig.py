f5aa7831c98a6fdb081c5aedf7b61ab11a2d5b20
Eduard Netšajev <themediaisajoke@gmail.com>
2014-10-01 07:08:26 +0300

    ITV0110 - BigPig statistics updated

--- Web design/ITV0110/Second/BigPig.py
+++ Web design/ITV0110/Second/BigPig.py
@@ -23,32 +23,42 @@ def play_game(bound1, bound2):
     current_score = 0
 
     turn = True  # True if it's first player turn, False otherwise
+    turns = 0
     while scores[0] < 100 and scores[1] < 100:
         rolled = roll()
 
         if rolled == 0:
             turn = not turn
             current_score = 0
+            turns = 0
+            continue
         else:
+            turns += 1
             current_score += rolled
 
         if turn and current_score > bound1:
             scores[0] += current_score
             turn = not turn
+            turns = 0
             current_score = 0
         if not turn and current_score > bound2:
             scores[1] += current_score
             turn = not turn
             current_score = 0
+            turns = 0
 
-    if scores[0] > 100:
+    if scores[0] >= 100:
         return True  # First player won
     else:
         return False  # First player lost to Second player
 
 
-def get_winrate(bound1, bound2, count):
+def get_winrate(bound1=100, bound2=100, count=10, turns1=100, turns2=100):
     won = 0.0
+    bound1 = int(bound1)
+    bound2 = int(bound2)
+    turns1 = int(turns1)
+    turns2 = int(turns2)
     for i in range(count):
         if play_game(bound1, bound2):
             won += 1
@@ -56,11 +66,11 @@ def get_winrate(bound1, bound2, count):
 
 
 def main():
-    bound1 = 20
+    bound1 = 2
     bound2 = 100
     count = 10000
     print("Playing {} times.\nFirst player rolls until {}.\nSecond player rolls until {}.".format(count, bound1, bound2))
-    print("Winrate of the first player: " + str(get_winrate(bound1, bound2, count)))
+    print("Winrate of the first player: " + str(get_winrate(0, 0, bound1, bound2, count)))
 
 if __name__ == "__main__":
     main()
--- Web design/ITV0110/Second/BigPigStatistics.py
+++ Web design/ITV0110/Second/BigPigStatistics.py
@@ -16,12 +16,12 @@ def get_wave_lenght_from_data_point(value, min_value, max_value):
 
     #   Data normalization: Convert data value in the
     #   range of min_values..max_values to the range 380..780
-    result = (value - min_value) / (max_value-min_value) * (max_wave - min_wave) + min_wave
+    result = (value - min_value) / (max_value+0.1-min_value) * (max_wave - min_wave) + min_wave
     return result
 
 
 def wave_length_to_rgb(wave_length):
-    gamma = 0.80
+    gamma = 0.950
     intensity_max = 255
 
     """ Taken from Earl F. Glynn's web page Spectra Lab Report:
@@ -29,25 +29,25 @@ def wave_length_to_rgb(wave_length):
     """
 
     if 380 <= wave_length < 781:
-        if wave_length < 440:
-            red = -(wave_length - 440) / (440 - 380)
+        if wave_length < 460:
+            red = -(wave_length - 460) / (460 - 380)
             green = 0.0
             blue = 1.0
-        elif wave_length < 490:
+        elif wave_length < 580:
             red = 0.0
-            green = (wave_length - 440) / (490 - 440)
+            green = (wave_length - 460) / (580 - 460)
             blue = 1.0
-        elif wave_length < 510:
+        elif wave_length < 620:
             red = 0.0
             green = 1.0
-            blue = -(wave_length - 510) / (510 - 490)
-        elif wave_length < 580:
-            red = (wave_length - 510) / (580 - 510)
+            blue = -(wave_length - 620) / (620 - 560)
+        elif wave_length < 690:
+            red = (wave_length - 570) / (690 - 570)
             green = 1.0
             blue = 0.0
-        elif wave_length < 645:
+        elif wave_length < 755:
             red = 1.0
-            green = -(wave_length - 645) / (645 - 580)
+            green = -(wave_length - 755) / (755 - 580)
             blue = 0.0
         else:
             red = 1.0
@@ -60,12 +60,12 @@ def wave_length_to_rgb(wave_length):
 
     # Let the intensity fall off near the vision limits
     if 380 <= wave_length:
-        if wave_length < 420:
-            factor = 0.3 + 0.7*(wave_length - 380) / (420 - 380)
-        elif wave_length < 701:
+        if wave_length < 500:
+            factor = 0.3 + 0.7*(wave_length - 380) / (500 - 380)
+        elif wave_length < 751:
             factor = 1.0
         elif wave_length < 781:
-            factor = 0.3 + 0.7*(780 - wave_length) / (780 - 700)
+            factor = 0.3 + 0.7*(780 - wave_length) / (780 - 750)
     else:
         factor = 0.0
 
@@ -76,35 +76,40 @@ def wave_length_to_rgb(wave_length):
 
     return rgb
 
+# real tries number = tries squared
+tries = 1
+
 
 def create_matrix(game_count=10):
-    matrix = [[_ for _ in range(100)] for _ in range(100)]
+    size = 100 * tries
+    matrix = [[_ for _ in range(size)] for _ in range(size)]
 
-    for i in range(100):
-        for j in range(100):
-            matrix[i][j] = BigPig.get_winrate(i, j, game_count)
+    for i in range(size):
+        for j in range(size):
+           #matrix[i][j] = BigPig.get_winrate(10000, 10000, game_count, i/tries, 15-j/tries)
+           # points vs. points
+           # matrix[i][j] = BigPig.get_winrate(i/tries, 100-j/tries, game_count)
+            matrix[i][j] = i
+        if i % 10 == 0:
+            print("done {} out of {}".format(i, size))
     return matrix
 
 
 def generate_heatmap(data_matrix):
 
-    size = 100
+    size = 100 * tries
     min_freq = 1
     max_freq = 0
 
-    def draw_heatmap_rgb(dataset, mode):
-        tile_size = 5
-        im = Image.new(mode, (size*tile_size, size*tile_size))
+    def draw_heatmap_rgb(dataset):
+        tile_size = 1
+        im = Image.new("RGB", (size*tile_size, size*tile_size))
         draw = ImageDraw.Draw(im)
         for d in dataset:
-            if mode == "RGB":
-                draw.rectangle([(d[0]*tile_size, d[1]*tile_size), ((d[0]+1)*tile_size, (d[1]+1)*tile_size)],
-                               wave_length_to_rgb(get_wave_lenght_from_data_point(d[2], min_freq, max_freq)))
-            elif mode == "L":
-                draw.rectangle([(d[0]*tile_size, d[1]*tile_size), ((d[0]+1)*tile_size, (d[1]+1)*tile_size)],
-                               255*((d[2]-min_freq)/(max_freq-min_freq)))
+            draw.rectangle([(d[0]*tile_size, d[1]*tile_size), ((d[0]+1)*tile_size, (d[1]+1)*tile_size)],
+                           wave_length_to_rgb(get_wave_lenght_from_data_point(d[2], min_freq, max_freq)))
         # write file
-        im.save("heatmap_%s.png" % mode, "PNG")
+        im.save("winrateGuideFromZeroToHundred.jpg", "PNG")
 
     data = []
 
@@ -118,14 +123,13 @@ def generate_heatmap(data_matrix):
 
     for i in range(size):
         for j in range(size):
-            data.append((j, i, data_matrix[i][j]))
+            data.append((i, j, data_matrix[i][j]))
 
-    draw_heatmap_rgb(data, "RGB")
-    draw_heatmap_rgb(data, "L")
+    draw_heatmap_rgb(data)
 
 
 def main():
-    generate_heatmap(create_matrix(10))
+    generate_heatmap(create_matrix(1000))
 
 if __name__ == "__main__":
     main()
