ee3d16a33ccc82e5c748e8d0c3c6687ed27b8747
Eduard Netšajev <themediaisajoke@gmail.com>
2014-10-10 18:36:21 +0300

    Location Tweets - Cache class documented, CheckStyle checked

--- Java/LocationTweets/Cache.java
+++ Java/LocationTweets/Cache.java
@@ -1,17 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
- * Cache functionality. The implementation
- * of ICache should be able to look information
- * from the cache file and add/update information
- * in the file.
- * @author Ago
- *
+ * Cache functionality which is able to
+ * look information from the cache file
+ * and add/update information in the file.
  */
 class Cache implements ICache {
 
+    /**
+     * Cache file name.
+     */
     String filename;
 
     /**
@@ -31,53 +37,61 @@ class Cache implements ICache {
         }
         File file = new File(filename);
         if (file.canRead()) {
-            try (BufferedReader in = new BufferedReader(new FileReader(file))){
+            try (BufferedReader in = new BufferedReader(new FileReader(file))) {
                 String s;
 
-                while((s = in.readLine()) != null){
+                while ((s = in.readLine()) != null) {
 
                     String[] data = s.split(",");
-                    for(String field: data){
+                    for (String field: data) {
                         if (location.equalsIgnoreCase(field)) {
-
-                            ITwitterQuery query = new TwitterQuery();
-                            query.setLocation(data[0]);
-                            try {
-                                query.setLatitude(Double.parseDouble(data[1]));
-                            } catch (Exception e) {
-                                // do nothing
-                            }
-                            try {
-                                query.setLongitude(Double.parseDouble(data[2]));
-                            } catch (Exception e) {
-                                // do nothing
-                            }
-                            try {
-                                query.setRadius(Double.parseDouble(data[3]));
-                            } catch (Exception e) {
-                                // do nothing
-                            }
-                            return query;
+                            return getQueryFromData(data);
                         }
                     }
                 }
                 in.close();
             } catch (IOException e) {
-                LocationTweets.out.println("Can't read cache from file " + filename);
+                LocationTweets.out.print("Can't read cache from file: ");
+                LocationTweets.out.println(filename);
             }
         }
         return null;
     }
 
     /**
-     * Updates an existing row in the cache file. Note: in the current set up,
-     * this method should add a new line if the location does not exist (e.g.
-     * you don't have anything to update). If you find it more convenient to
-     * have add and update functionalities separately, implement addLocation
-     * method and call them accordingly to the situtaion.
+     * Given array of strings with the data,
+     * creates an instance of TwitterQuery.
+     * @param data String array of data about the query
+     * @return TwitterQuery instance with filled fields
+     */
+    private ITwitterQuery getQueryFromData(String[] data) {
+        ITwitterQuery query = new TwitterQuery();
+        query.setLocation(data[0]);
+        try {
+            query.setLatitude(Double.parseDouble(data[1]));
+        } catch (Exception e) {
+            query.setLatitude(0.0);
+        }
+        try {
+            query.setLongitude(Double.parseDouble(data[2]));
+        } catch (Exception e) {
+            query.setLongitude(0.0);
+        }
+        try {
+            query.setRadius(Double.parseDouble(data[3]));
+        } catch (Exception e) {
+            query.setRadius(0.0);
+        }
+        return query;
+    }
+
+    /**
+     * Updates an existing row in the cache file.
+     * If the location doesn't exist in the file,
+     * add it as a new row at the end of the file.
      *
-     * @param query Query parameters instances which has all the necessary
-     *              values.
+     * @param query ITwitterQuery instance which
+     *              has all the necessary values.
      */
     @Override
     public void updateLocation(ITwitterQuery query) {
@@ -94,14 +108,14 @@ class Cache implements ICache {
         }
 
         if (file.canRead() && file.canWrite()) {
-            try (BufferedReader in = new BufferedReader(new FileReader(file))){
+            try (BufferedReader in = new BufferedReader(new FileReader(file))) {
                 boolean foundInCache = false;
                 String s;
                 String d;
                 List<String> content = new ArrayList<String>();
                 String location = query.getLocation();
 
-                while((s = in.readLine()) != null){
+                while ((s = in.readLine()) != null) {
 
                     String[] data = s.split(",");
 
@@ -129,12 +143,13 @@ class Cache implements ICache {
                 in.close();
 
                 PrintWriter f2 = new PrintWriter(new FileWriter(file, false));
-                for(String string : content) {
+                for (String string : content) {
                     f2.println(string);
                 }
                 f2.close();
             } catch (Exception e) {
-                LocationTweets.out.println("Can't use cache in file "+filename);
+                LocationTweets.out.print("Can't use cache in file ");
+                LocationTweets.out.println(filename);
             }
         }
     }
@@ -142,11 +157,11 @@ class Cache implements ICache {
     /**
      * Sets the filename where the cache is stored.
      *
-     * @param filename Filename where the cache is stored
+     * @param newFilename Filename where the cache is stored
      */
     @Override
-    public void setCacheFilename(String filename) {
-        this.filename = filename;
+    public void setCacheFilename(String newFilename) {
+        filename = newFilename;
     }
 
     /**
