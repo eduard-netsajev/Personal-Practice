c027a8597ed4ea4401860ffd2d461523892b1bf7
Eduard Netšajev <themediaisajoke@gmail.com>
2014-10-16 15:26:29 +0300

    HKNProblem solved, 100/100

--- Java/HackerRank/HKNProblem.java
+++ Java/HackerRank/HKNProblem.java
@@ -14,10 +14,50 @@ class HKNProblemSolution {
             long a = Long.parseLong(tokenizer.nextToken());
             long b = Long.parseLong(tokenizer.nextToken());
 
+
             int count = 0;
-            for (; a <= b; a++) {
-                if (is_palbin(a)) {
-                    count++;
+
+            int len1 = Long.SIZE - Long.numberOfLeadingZeros(a);
+            int len2 = Long.SIZE - Long.numberOfLeadingZeros(b);
+
+            if (len2 > len1) {
+
+                int bound1 = 1 << (len1);
+                int bound2 = 1 << (len2 - 1);
+
+                if (Long.bitCount(a) == 1) {
+                    len1--;
+                } else {
+                    for (; a < bound1; a++) {
+
+                        if (is_palbin(a)) {
+                            count++;
+                        }
+                    }
+                }
+
+                if (Long.bitCount(b) == len2) {
+                    len2++;
+                } else {
+                    for (; bound2 <= b; bound2++) {
+                        if (is_palbin(bound2)) {
+                            count++;
+                        }
+                    }
+                }
+
+                int x;
+                for (++len1; len1 < len2; len1++) {
+                    x = (int) (len1 / 2.0 - 0.5);
+                    count += 1 << x;
+
+                }
+
+            } else {
+                for (; a <= b; a++) {
+                    if (is_palbin(a)) {
+                        count++;
+                    }
                 }
             }
             System.out.println(count);
