9ae7c20888921f8d70fcb675e8f7cb053af09e2e
Eduard Netšajev <themediaisajoke@gmail.com>
2014-10-18 02:22:19 +0300

    IEEEXtreme Reader prepared

---
+++ Java/IEEEXtreme/src/Reader.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Reader class for general input reading.
+ */
+class Reader {
+    /**
+     * BufferedReader instance.
+     */
+    static BufferedReader reader;
+
+    /**
+     * StringTokenizer instance.
+     */
+    static StringTokenizer tokenizer;
+
+    /**
+     * Currently being used line.
+     */
+    static String currentLine;
+
+    /**
+     * Call this method to initialize reader for InputStream.
+     *
+     * @param input InputStream instance
+     */
+    static void init(InputStream input) {
+        reader = new BufferedReader(new InputStreamReader(input));
+        tokenizer = new StringTokenizer("");
+        currentLine = "";
+    }
+
+    /**
+     * Clear both tokenizer and current line.
+     */
+    static void flushTokenizer() {
+        currentLine = "";
+        tokenizer = new StringTokenizer(currentLine);
+    }
+
+    /**
+     * Get next token (word).
+     * @return next token
+     * @throws IOException if any problems happen
+     */
+    static String next() throws IOException {
+        while (!tokenizer.hasMoreTokens()) {
+            currentLine = reader.readLine();
+            tokenizer = new StringTokenizer(currentLine);
+        }
+        return tokenizer.nextToken();
+    }
+
+    /**
+     * Grab whole next line. Put it into the tokenizer.
+     * @return next line
+     */
+    static String nextLine() {
+        // probably not even needed here
+        flushTokenizer();
+        try {
+            currentLine = reader.readLine();
+            tokenizer = new StringTokenizer(currentLine);
+            return currentLine;
+        } catch (IOException|NullPointerException e) {
+            return null;
+        }
+    }
+}
